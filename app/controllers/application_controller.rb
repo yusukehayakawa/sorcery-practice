3010c2439608e9091ef79321e74d1ab5465c65e1
HayakawaYusuke <yusukeeee0412@gmail.com>
2017-09-15 15:55:10 -0400

    add method for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_login
+
+  private
+  def not_authenticated
+    redirect_to new_session_path unless logged_in?
+  end
 end
